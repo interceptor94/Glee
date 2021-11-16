43675fb66eb3e56c1563d75107cb0b0e18007d15
interceptor94 <dgei@inbox.ru>
2021-11-16 13:47:19 +0300

    добавил слайдер

--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,7 @@ function images() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
