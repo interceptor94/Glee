7fdb78442fe4cc7c51aeffad2d4e303bf94a9f58
interceptor94 <dgei@inbox.ru>
2021-12-03 14:48:08 +0300

    добавил страницу с товарами без фильтра плиток

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,39 @@
 $(function() {
+  $(".star").rateYo({
+    starWidth: "13px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00", 
+    readOnly: true
+  });
+
+  $(".shop-filter__price-input").ionRangeSlider({
+    prefix: "$",
+    onStart: function (data){
+      $('.form-from').text(data.from);
+      $('.form-to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.form-from').text(data.from);
+      $('.form-to').text(data.to);
+    },
+  });
+
+
   $('.top-slider__inner').slick({
     arrows: false,
     dots: true
-  });
+  }); 
+  
+  
 
   $(".filter-link").click(function () {
     $(".filter-link").removeClass("filter-link--active");
     $(this).addClass("filter-link--active");
   });
-
+  
   var mixer = mixitup('.product-items');
   var mixer = mixitup('.design-items');
 
+  
 
 });
