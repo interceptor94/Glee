acf176247bd192943740e0a44db5dfa95a641ec1
interceptor94 <dgei@inbox.ru>
2021-12-07 18:28:34 +0300

    added page blog

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,23 @@
 $(function() {
-
-
-
-
-
-
+  // $('.product-one__thumb').slick({
+  //   asNavFor: '.product-one__big',
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   vertical: true,
+  //   draggable: false
+  // });
+  // $('.product-one__big').slick({
+  //   asNavFor: '.product-one__thumb',
+  //   draggable: false,
+    
+  // });
   $(".star").rateYo({
     starWidth: "13px",
     normalFill: "#d6d6d6",
     ratedFill: "#ffcc00", 
     readOnly: true
   });
-
-  $(".shop-filter__price-input").ionRangeSlider({
+  $('.shop-filter__price-input').ionRangeSlider({
     prefix: "$",
     onStart: function (data){
       $('.form-from').text(data.from);
@@ -21,25 +26,16 @@ $(function() {
     onChange: function (data) {
       $('.form-from').text(data.from);
       $('.form-to').text(data.to);
-    },
+    }
   });
-
 
   $('.top-slider__inner').slick({
     arrows: false,
     dots: true
   }); 
-  
-  
 
   $(".filter-link").click(function () {
     $(".filter-link").removeClass("filter-link--active");
     $(this).addClass("filter-link--active");
   });
-  
-  var mixer = mixitup('.product-items');
-  var mixer = mixitup('.design-items');
-
-  
-
 });
